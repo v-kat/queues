915be79acb9f0f083c3df9fb66e7bb1928ddc807
Ivy Rogatko <ipr210lehigh@gmail.com>
2020-02-18 18:51:26 -0800

    real-time queue

--- Queue.hs
+++ Queue.hs
@@ -1,15 +1,36 @@
 module Queue where
 
 import qualified Data.List as List
-import Control.Exception(assert)
+import GHC.Exts(IsList(..))
+--import Control.Exception(assert)
+------------------------------------------------------------------
+data StrictList a
+  = StrictNil
+  | StrictCons !a !(StrictList a)
 
+instance Functor StrictList where
+  fmap f = go where
+    go StrictNil         = StrictNil
+    go (StrictCons x xs) = StrictCons (f x) (go xs)
+------------------------------------------------------------------
 class Queue f where
   emptyQueue :: f a
   dequeue :: f a -> Maybe (a, f a)
   enqueue :: f a -> a -> f a
+
+queue2List :: Queue f => f a -> [a]
+queue2List xs = case dequeue xs of
+  Nothing -> []
+  Just (y, ys) -> y : queue2List ys
+
+list2Queue :: Queue f => [a] -> f a
+list2Queue = foldl (\acc a -> enqueue acc a) emptyQueue
 ------------------------------------------------------------------
 newtype ListQueue a = ListQueue { unListQueue :: [a] }
   deriving (Show)
+
+instance Functor ListQueue where
+  fmap f (ListQueue list) = ListQueue $ fmap f list
 
 instance Queue ListQueue where
   emptyQueue = ListQueue []
@@ -22,6 +43,9 @@ instance Queue ListQueue where
 data PairedLists a = PairedLists [a] [a]
   deriving (Show)
 
+instance Functor PairedLists where
+  fmap f (PairedLists l r) = PairedLists (fmap f l) (fmap f r)
+
 instance Queue PairedLists where
   emptyQueue = PairedLists [] []
   dequeue (PairedLists [] ys) = case List.reverse ys of
@@ -29,6 +53,53 @@ instance Queue PairedLists where
     x : xs -> Just (x, PairedLists xs [])
   dequeue (PairedLists (x : xs) ys) = Just (x, PairedLists xs ys)
   enqueue (PairedLists xs ys) a = PairedLists xs (a : ys)
-
 ------------------------------------------------------------------
--- TODO write rotate
+naiveRotate :: [a] -> [a] -> [a]
+naiveRotate xs ys = xs <> List.reverse ys
+
+-- | rotate L R A
+-- Invariant:
+-- |R| = |L| + 1
+rotate :: [a] -> [a] -> [a] -> [a]
+rotate [      ] [y     ] zs = y : zs
+rotate (x : xs) (y : ys) zs = x : rotate xs ys (y : zs)
+rotate _ _ _ = error "rotate: invariant violated"
+
+-- | Dequeue is worst case O(log n)
+data LazyPairedLists a = LazyPairedLists Int [a] Int [a]
+  deriving (Show)
+
+sizeLPLQueue :: LazyPairedLists a -> Int
+sizeLPLQueue (LazyPairedLists n _ m _) = n + m
+
+makeLPLQueue :: Int -> [a] -> Int -> [a] -> LazyPairedLists a
+makeLPLQueue sl l sr r
+  | sr <= sl = LazyPairedLists sl l sr r
+  | sr == sl + 1 = LazyPairedLists (sl + sr) (rotate l r []) 0 []
+  | otherwise = error "size invariant violated"
+
+instance Queue LazyPairedLists where
+  emptyQueue = LazyPairedLists 0 [] 0 []
+  dequeue (LazyPairedLists 0 [] 0 []) = Nothing
+  dequeue (LazyPairedLists n (x : xs) m ys) = Just (x, makeLPLQueue (n - 1) xs m ys)
+  dequeue _ = error "dequeue failed"
+  enqueue (LazyPairedLists n xs m ys) a = makeLPLQueue n xs (m + 1) (a : ys)
+
+instance IsList (LazyPairedLists a) where
+  type Item (LazyPairedLists a) = a
+  fromList = foldl enqueue emptyQueue
+  toList (LazyPairedLists _ xs _ ys) = naiveRotate xs ys
+------------------------------------------------------------------
+data RealTimeQueue a = RealTimeQueue [a] [a] [a]
+  deriving (Eq, Show)
+
+makeRTQueue :: [a] -> [a] -> [a] -> RealTimeQueue a
+makeRTQueue as bs cs = case cs of
+  [] -> let x = rotate as bs [] in RealTimeQueue x [] x
+  _ : xs -> RealTimeQueue as bs xs
+
+instance Queue RealTimeQueue where
+  emptyQueue = RealTimeQueue [] [] []
+  dequeue (RealTimeQueue (x : xs) ys zs) = Just (x, makeRTQueue xs ys zs)
+  dequeue (RealTimeQueue [] _ _) = Nothing
+  enqueue (RealTimeQueue xs ys zs) a = makeRTQueue xs (a : ys) zs
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,11 +1,33 @@
+{-# language OverloadedStrings, AllowAmbiguousTypes #-}
+
 module Main where
 
 import Queue
-import Test.Hspec
--- import Test.Hspec.QuickCheck
-import Test.QuickCheck
-import Data.Primitive.Array
-import Data.Word
-import Data.Bits
-import GHC.Exts
+import Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+import Data.Functor
 
+main :: IO ()
+main = void tests
+
+prop_reverse :: Property
+prop_reverse =
+  property $ do
+    xs <- forAll $ Gen.list (Range.linear 0 100) Gen.alpha
+    reverse (reverse xs) === xs
+
+prop_list_queue_tripping :: forall f. Queue f => Property
+prop_list_queue_tripping = property $ do
+  xs <- forAll $ Gen.list (Range.linear 0 100) (Gen.enumBounded @Gen @Int)
+  queue2List @f (list2Queue xs) === xs
+
+tests :: IO Bool
+tests =
+  checkParallel $ Group "Test.Example"
+  [ ("prop_reverse", prop_reverse)
+  , ("prop_list_queue_tripping: ListQueue"  , prop_list_queue_tripping @ListQueue)
+  , ("prop_list_queue_tripping: PairedLists", prop_list_queue_tripping @PairedLists)
+  , ("prop_list_queue_tripping: LazyPairedLists", prop_list_queue_tripping @LazyPairedLists)
+  , ("prop_list_queue_tripping: RealTimeQueue", prop_list_queue_tripping @RealTimeQueue)
+  ]
